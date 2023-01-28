bcf60afe1c61bffe5fc20b5ae6fd28c559848a85
DinoVrtlar <79018897+steal4life@users.noreply.github.com>
2023-01-28 21:47:53 +0100

    Add files via upload

---
+++ TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Projektic
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public DateTime date { get; set; }
+    }
+}
